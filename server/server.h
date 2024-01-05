5a1cc72e27176d8bf569253b0ee6c36bcceaeb1c
imankulova_dana <imankulova_dana@cybersec74.ru>
2024-01-05 21:12:55 +0500

    added class Server()

--- server/server.h
+++ server/server.h
@@ -7,6 +7,13 @@
 
 #include <string>
 #include <vector>
+#include <sstream>
+
+
+#define PORT 8081
+#define BUFFER_SIZE 1024
+#define MAX_CLIENTS 10
+
 
 class Server {
 
@@ -15,6 +22,13 @@ public:
     std::vector<std::string> list_files(std::string directory);
     std::vector<std::string> list_processes(std::string directory);
 
+};
+
+
+class ServerHandler {
+public:
+    static void* handle_client(void* client_socket_ptr);
+    static void run_server();
 };
 
 
